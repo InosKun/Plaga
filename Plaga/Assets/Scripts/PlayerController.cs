1f6fd2fc03f3ac29cd8f56d05f6a55a3161d040d
InosKun <moracastanosmartin@gmail.com>
2025-05-15 12:06:23 +0200

    NPC Maria

--- Plaga/Assets/Scripts/PlayerController.cs
+++ Plaga/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     private Rigidbody2D rb;
     private CapsuleCollider2D capsuleCollider;
+    public LayerMask Interactable;
     public LayerMask solidObject;
 
     private Vector2 input;
@@ -28,6 +29,23 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("moveX", input.x);
         animator.SetFloat("moveY", input.y);
         animator.SetBool("isMoving", input != Vector2.zero);
+
+        if (Input.GetKeyDown(KeyCode.E))
+            Interact();
+    }
+
+    void Interact()
+    {
+        var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
+        var interactPos = transform.position + facingDir;
+
+        Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
+
+        var collider = Physics2D.OverlapCircle(interactPos, 0.2f, Interactable);
+        if (collider != null)
+        {
+            Debug.Log("NPC HERE");
+        }
     }
 
     private void FixedUpdate()
