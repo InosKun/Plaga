90e0eeaef5f303f6613d77549c602bda6684c91d
InosKun <moracastanosmartin@gmail.com>
2025-06-03 23:07:03 +0200

    Integrando el splash

---
+++ Plaga/Assets/Scripts/UI/MenuPPal.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+enum BotonesMenuPpal { MPPal_Play,
+MMPal_Carga,
+MPPal_Option,
+MPPal_Credits,
+MPPal_Exit,
+MPPal_TotalBotones }
+
+public class MenuPpal : MonoBehaviour
+{
+
+    //Make sure to attach these Buttons in the Inspector
+     [SerializeField]
+     string[] nombreBoton = {
+        "Nueva",
+        "Cargar",
+        "Options",
+        "Creditos",
+        "Salir"};
+
+      Button[] boton;
+
+    void Start () {
+        boton = new Button [(int) BotonesMenuPpal.MPPal_TotalBotones];
+
+        for (int i = (int)BotonesMenuPpal.MPPal_Play; 
+        i < (int)BotonesMenuPpal.MPPal_TotalBotones;
+        i++)
+        boton[i] = GameObject.Find(nombreBoton[i]).GetComponent<Button>();
+
+        boton[(int)BotonesMenuPpal.MPPal_Play].onClick.AddListener(playClicked);
+        boton[(int)BotonesMenuPpal.MMPal_Carga].onClick.AddListener(cargaClicked);
+        boton[(int)BotonesMenuPpal.MPPal_Option].onClick.AddListener(optionsClicked); 
+        boton[(int)BotonesMenuPpal.MPPal_Credits].onClick.AddListener(creditsClicked);
+        boton[(int)BotonesMenuPpal.MPPal_Exit].onClick.AddListener(exitClicked);
+        }
+      
+
+    void exitClicked() {
+         Debug.Log("Botón Exit presionado");
+     Application.Quit(); //Se cierra la aplicación
+
+     #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Detener el editor
+     #endif
+    }
+
+     void playClicked() {
+//Output this to console when the Button is clicked
+       Debug.Log("You have clicked the button Jugar!");
+       SceneManager.LoadScene("first person");
+    }
+
+    void cargaClicked()
+    {
+        //Output this to console when the Button is clicked
+        Debug.Log("You have clicked the button Jugar!");
+    }
+
+    void optionsClicked() {
+      //Output this to console when the Button is clicked
+      SceneManager.LoadScene("opciones");
+    }  
+
+      void creditsClicked() {
+      //Output this to console when the Button is clicked
+      SceneManager.LoadScene("Creditos");
+    }  
+
+     void Update() {
+      //Regla del escape
+      if (Input.GetKey("escape"))
+        exitClicked();
+      //Regla del enter
+      if (Input.GetKey(KeyCode.Return))
+        playClicked();
+    }
+
+}
+
---
+++ Plaga/Assets/Scripts/UI/Volumnen.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+enum BotonVolumen {
+Vol_Volver,
+Vol_TotalBotones }
+
+public class Volumnen : MonoBehaviour
+{
+
+    [SerializeField]
+     string[] nombreBoton = {
+        "Volver"
+        };
+
+      Button[] boton;
+    // Start is called before the first frame update
+    void Start()
+    {  boton = new Button [(int) BotonVolumen.Vol_TotalBotones];
+
+        for (int i = (int)BotonVolumen.Vol_Volver; 
+        i < (int)BotonVolumen.Vol_TotalBotones;
+        i++)
+        boton[i] = GameObject.Find(nombreBoton[i]).GetComponent<Button>();
+
+        boton[(int)BotonVolumen.Vol_Volver].onClick.AddListener(volverClicked);
+        }
+      
+        
+    
+
+    // Update is called once per frame
+  
+       void volverClicked() {
+         Debug.Log("Botón Volver presionado");
+         SceneManager.LoadScene("opciones");
+          }
+    
+}
